65da50bc13e4d0d82cd2dc353078ad3a814ad180
Kris Petersen <kris@neighborhuddle.com>
2017-01-03 16:24:13 -0600

    Add tests for GET /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,10 +4,20 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
-//Clear the test database for each test. Note it will run before every test, so count is 0 at the start. DON'T use prod DB here as it will get wiped.
+const todos = [{
+	text: 'First test todo'
+}, {
+	text: 'Second test todo'
+}];
+
+
+
+//Clear the test database for each test then load in some reliable sample data. Note it will run before every test, so count is 0 at the start. DON'T use prod DB here as it will get wiped.
 beforeEach((done) => {
-	Todo.remove({}).then(() => done());
-})
+	Todo.remove({}).then(() => {
+		return Todo.insertMany(todos);
+	}).then(() => done());
+});
 
 describe('POST /todos', () => {
 	it('should create a new todo', (done) => {
@@ -26,7 +36,7 @@ describe('POST /todos', () => {
 					return done(err);
 				}
 
-				Todo.find().then((todos) => {
+				Todo.find({text}).then((todos) => {
 					expect(todos.length).toBe(1);
 					expect(todos[0].text).toBe(text);
 					done();
@@ -47,10 +57,21 @@ describe('POST /todos', () => {
 				}
 
 				Todo.find().then((todos) => {
-					expect(todos.length).toBe(0);
+					expect(todos.length).toBe(2); //Use 2 because we have two to start in sample data
 					done();
 				}).catch((e) => done(e));		
 			});
 	});
 });
 
+describe('GET /todos', () => {
+	it('should get all todos', (done) => {
+		request(app)
+			.get('/todos')
+			.expect(200)
+			.expect((res) => {
+				expect(res.body.todos.length).toBe(2);
+			})
+			.end(done); //no need to add on a function to end() because we're not doing anything asynchronously
+	});
+});
