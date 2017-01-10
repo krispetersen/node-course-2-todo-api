a2047a1d540c466c155b485d6d9a295a30419532
Kris Petersen <kris@neighborhuddle.com>
2017-01-10 14:05:36 -0600

    Update config to use json file

--- server/config/config.js
+++ server/config/config.js
@@ -1,11 +1,11 @@
 //none of this matters on Heroku as they set value of 'production' by default
-
 var env = process.env.NODE_ENV || 'development';
 
-if (env === 'development') {
-	process.env.PORT = 3000;
-	process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
-} else if (env === 'test') {
-	process.env.PORT = 3000
-	process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+if (env === 'development' || env === 'test') {
+	var config = require('./config.json'); //when including a JSON file, it's automatically converted into a javascript object
+	var envConfig = config[env]; //bracket notation
+
+	Object.keys(envConfig).forEach((key) => {
+		process.env[key] = envConfig[key];
+	});
 }
